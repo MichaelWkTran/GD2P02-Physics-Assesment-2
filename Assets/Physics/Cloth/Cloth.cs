b160f43a51e8884559e282b4af11b09cc6fb7ebc
MichaelWkTran <Michael.wktran@gmail.com>
2022-10-21 23:14:31 +1300

    Update

--- Assets/Physics/Cloth/Cloth.cs
+++ Assets/Physics/Cloth/Cloth.cs
@@ -19,6 +19,12 @@ public class Cloth : MonoBehaviour
         return (_cellY * ((int)width + 1)) + _cellX;
     }
 
+    public void GetCellPos(int _vertexIndex, out int _cellX, out int _cellY)
+    {
+        _cellY = (int)(_vertexIndex/(width + 1.0f));
+        _cellX = _vertexIndex - _cellY;
+    }
+
     public void GenerateGrid()
     {
         Vector3[] verticies = new Vector3[(width + 1) * (height + 1)];
@@ -36,16 +42,19 @@ public class Cloth : MonoBehaviour
             for (int cellX = 0; cellX < width + 1; cellX++)
             {
                 //Set vertex position
-                verticies[GetVertexIndex(cellX, cellY)] = new Vector3(cellX * cellSize.x, cellY * cellSize.y);
+                Vector3 vertexPosition = new Vector3(cellX*cellSize.x, -cellY*cellSize.y);
+                vertexPosition += new Vector3(-width*cellSize.x, height*cellSize.y) / 2.0f;
+                verticies[GetVertexIndex(cellX, cellY)] = vertexPosition;
 
                 //Create a game object to control the position of the vertex
                 GameObject vertexGameObject = new GameObject("ClothParticle" + cellX + "-" + cellY);
                 vertexGameObject.transform.parent = transform;
-                vertexGameObject.transform.localPosition = verticies[GetVertexIndex(cellX, cellY)];
+                vertexGameObject.transform.localPosition = vertexPosition;
 
                 //Give the created gameObject the ClothParticle script and set its properties
                 ClothParticle vertexParticle = vertexGameObject.AddComponent<ClothParticle>();
                 particles[GetVertexIndex(cellX, cellY)] = vertexParticle;
+                
                 vertexParticle.cloth = this;
                 vertexParticle.cellX = (uint)cellX;
                 vertexParticle.cellY = (uint)cellY;
@@ -57,15 +66,38 @@ public class Cloth : MonoBehaviour
             for (int cellX = 0; cellX < width; cellX++)
             {
                 indices[index] =     GetVertexIndex(cellX,   cellY);
-                indices[index + 1] = GetVertexIndex(cellX,   cellY+1);
-                indices[index + 2] = GetVertexIndex(cellX+1, cellY);
+                indices[index + 1] = GetVertexIndex(cellX + 1, cellY);
+                indices[index + 2] = GetVertexIndex(cellX,   cellY+1);
 
                 indices[index + 3] = GetVertexIndex(cellX,   cellY+1);
-                indices[index + 4] = GetVertexIndex(cellX+1, cellY+1);
-                indices[index + 5] = GetVertexIndex(cellX+1, cellY);
+                indices[index + 4] = GetVertexIndex(cellX + 1, cellY);
+                indices[index + 5] = GetVertexIndex(cellX+1, cellY+1);
 
                 index += 6U;
             }
+
+        //Set the joints of the particles
+        foreach (ClothParticle particle in particles)
+        {
+            ClothParticle GetSurroundingParticle(int _cellOffsetX, int _cellOffsetY)
+            {
+                if (particle.cellX + _cellOffsetX < 0) return null;
+                if (particle.cellX + _cellOffsetX > width) return null;
+                if (particle.cellY + _cellOffsetY < 0) return null;
+                if (particle.cellY + _cellOffsetY > height) return null;
+
+                return particles[GetVertexIndex((int)particle.cellX + _cellOffsetX, (int)particle.cellY + _cellOffsetY)];
+            }
+
+            particle.top         = GetSurroundingParticle( 0,-1);
+            particle.topRight    = GetSurroundingParticle( 1,-1);
+            particle.right       = GetSurroundingParticle( 1, 0);
+            particle.bottomRight = GetSurroundingParticle( 1, 1);
+            particle.bottom      = GetSurroundingParticle( 0, 1);
+            particle.bottomLeft  = GetSurroundingParticle(-1, 1);
+            particle.left        = GetSurroundingParticle(-1, 0);
+            particle.leftTop     = GetSurroundingParticle(-1, 1);
+        }
 
         //Set to mesh
         mesh.vertices = verticies;
--- Assets/Physics/Cloth/ClothParticle.cs
+++ Assets/Physics/Cloth/ClothParticle.cs
@@ -10,6 +10,14 @@ public class ClothParticle : MonoBehaviour
     public uint cellY;
     public int vertexIndex { get; private set; }
 
+    [Header("Physics")]
+    Vector3 prevPosition;
+    Vector3 acceleration;
+    float mass = 1.0f;
+    float gravityScale = 0.01f;
+
+    [Header("Constraints")]
+    public bool pinned = false;
     public ClothParticle top;
     public ClothParticle topRight;
     public ClothParticle right;
@@ -21,7 +29,7 @@ public class ClothParticle : MonoBehaviour
 
     void OnDestroy()
     {
-        ////If there are particles this particle is joined to. Then ignore checking the mesh triangles
+        //If there are particles this particle is joined to. Then ignore checking the mesh triangles
         //if (!top && !topRight && !right && !bottomRight && !bottom && !bottomLeft && !left && !leftTop)
         //    return;
 
@@ -64,16 +72,37 @@ public class ClothParticle : MonoBehaviour
 
     void Start()
     {
+        prevPosition = transform.position;
         vertexIndex = cloth.GetVertexIndex((int)cellX, (int)cellY);
     }
 
     void Update()
     {
         //Check whether there are no particles this particle is joined to. If so delete it.
-        //if (!top && !topRight && !right && !bottomRight && !bottom && !bottomLeft && !left && !leftTop)
-        //{
-        //    Destroy(gameObject);
-        //    return;
-        //}
+        if (!top && !topRight && !right && !bottomRight && !bottom && !bottomLeft && !left && !leftTop)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //Ignore physics if pinned
+        if (pinned) return;
+
+        //Apply constraints
+
+
+        //Apply gravity
+        ApplyForce(Physics.gravity * gravityScale * mass);
+
+        //Apply verlet integration
+        Vector3 velocity = transform.position - prevPosition;
+        prevPosition = transform.position;
+        transform.position = transform.position + velocity + (acceleration * Time.deltaTime * Time.deltaTime);
+        acceleration = Vector3.zero;
+    }
+
+    void ApplyForce(Vector3 _force)
+    {
+        acceleration += _force / mass;
     }
 }
